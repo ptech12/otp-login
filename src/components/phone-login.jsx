b150a792a3a8c01f7e9ed3995d2991891e0650da
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-20 12:48:45 +0530

    OTP form and validation DONE

--- src/components/phone-login.jsx
+++ src/components/phone-login.jsx
@@ -1,33 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const OTPForm = () => {
   // using SetState for phoneNumber variable
-  const [phoneNumber, setPhoneNumber] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [showOtpField, setShowOtpField] = useState(false);
 
   // function to handle phone number's
-  const handlPhoneNumber = e => {
-    setPhoneNumber(e.target.value)
-
-  }
-  // function to handle submt 
-  const handlPhoneSubmit = e => {
+  const handlPhoneNumber = (e) => {
+    setPhoneNumber(e.target.value);
+  };
+  // function to handle submt
+  const handlPhoneSubmit = (e) => {
     e.preventDefault();
-    // alert(phoneNumber)
-  }
+
+    // phone number validation
+    const regex = /[^0-9]/g;
+    if (phoneNumber.length < 10 || regex.test(phoneNumber)) {
+      alert("Invalid Phone Number");
+    } else {
+      // call the backend API
+      // show OTP field
+      setShowOtpField(true);
+    }
+  };
 
   return (
     <div>
-      <form onSubmit={handlPhoneSubmit}>
-        <input 
-          type="text"
-          value={phoneNumber}
-          placeholder='Enter Phone Number'
-          onChange={handlPhoneNumber}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      {!showOtpField ? (
+        <form onSubmit={handlPhoneSubmit}>
+          <input
+            type="text"
+            value={phoneNumber}
+            placeholder="Enter Phone Number"
+            onChange={handlPhoneNumber}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <div>
+          <p>Enter OTP sent to {phoneNumber} </p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default OTPForm;
