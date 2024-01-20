acf2cfe2ca99790453595e690f0ca362a48d922e
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-20 12:40:11 +0530

    form markup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import OTPForm from './components/phone-login'
-
+import './App.css'
 function App() {
   return (
     <div className='App'>
--- src/components/phone-login.jsx
+++ src/components/phone-login.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const OTPForm = () => {
+  // using SetState for phoneNumber variable
+  const [phoneNumber, setPhoneNumber] = useState("")
+
+  // function to handle phone number's
+  const handlPhoneNumber = e => {
+    setPhoneNumber(e.target.value)
+
+  }
+  // function to handle submt 
+  const handlPhoneSubmit = e => {
+    e.preventDefault();
+    // alert(phoneNumber)
+  }
+
   return (
-    <div>OTPForm</div>
+    <div>
+      <form onSubmit={handlPhoneSubmit}>
+        <input 
+          type="text"
+          value={phoneNumber}
+          placeholder='Enter Phone Number'
+          onChange={handlPhoneNumber}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
 
