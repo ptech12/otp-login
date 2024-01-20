c05fdb5556ee1a137b98e1ccd705eda9458f7b62
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-20 14:18:25 +0530

    OTP form component

--- src/components/otp-input.jsx
+++ src/components/otp-input.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
-function OtpInput() {
-  return (
-    <div>OtpInput</div>
-  )
-}
+const OtpInput = ({ length = 4, onOtpSubmit = () => {} }) => {
+	const [otp, setOtp] = useState(new Array(length).fill(""));
+	const inputRefs = useRef([]);
+
+	/**
+	 * useEffect Hook
+	 * Whenever a component is rendered
+	 * the useEffect hook is called
+	 */
+	useEffect(() => {
+		if (inputRefs.current[0]) {
+			inputRefs.current[0].focus();
+		}
+	}, [])
+	
+	
+
+	
+
+	const handleChange = () => {};
+	const handleClick = () => {};
+	const handleKeyDown = () => {};
+
+
+
+
+	return <div>
+		{
+			otp.map((value, index) => {
+				return <input
+				key={index}
+				type="text"
+				ref={(input) => inputRefs.current[index] = input} 
+				value={value}
+				onChange={(e) => handleChange(index, e)}
+				onClick={() => handleClick(index)}
+				onKeyDown={(e) => handleKeyDown(index, e)}
+				className="otp-input"
+				/>
+
+			})
+		}
+	</div>;
+};
 
 export default OtpInput;
